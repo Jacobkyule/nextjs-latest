26d15aef11a9a0d20372dce41679d07d0674b5e9
Jacob Kyule <jacobkyule76@gmail.com>
2023-09-25 13:14:37 +0300

    typos

--- app/about/page.js
+++ app/about/page.js
@@ -6,21 +6,21 @@ export default function About() {
         <div className={styles.aboutWrapper}>    
         <div className={styles.about}>
         <h2>about me!</h2>
-<p>Hello there! I'm Jacob Kyule, a passionate Front-End Software Developer with a unique blend of technicalexpertise and a foundation in social science. Based in the vibrant tech hub of Nairobi, Kenya, I bring a diverseperspective to the world of web development.</p>
+<p>Hello there! I&apos;m Jacob Kyule, a passionate Front-End Software Developer with a unique blend of technical expertise and a foundation in social science. Based in the vibrant tech hub of Nairobi, Kenya, I bring a diverse perspective to the world of web development.</p>
 <br/>
-<p>With a Bachelor's degree in Sociology, I have a deep appreciation for understanding human behavior andsociety's intricacies. This background complements my technical skills, allowing me to create user-centric digitalexperiences that resonate with people on a profound level.</p>
+<p>With a Bachelor&apos;s degree in Sociology, I have a deep appreciation for understanding human behavior and society&apos;s intricacies. This background complements my technical skills, allowing me to create user-centric digital experiences that resonate with people on a profound level.</p>
 <br/>
-<p>Armed with a year of hands-on experience in web development, I've embarked on a journey of creativity andinnovation, weaving digital magic through code. My growing portfolio boasts over 10 captivating projects, each atestament to my dedication to crafting seamless digital experiences.</p>
+<p>Armed with a year of hands-on experience in web development, I&apos;ve embarked on a journey of creativity and innovation, weaving digital magic through code. My growing portfolio boasts over 10 captivating projects, each a testament to my dedication to crafting seamless digital experiences.</p>
 <br/>
 <p>Allow me to highlight my top four recent achievements:</p>
 <br/>
-<p>1## News Reader Project: I've brought the world of news to life through a React.js web app, masterfully handlingUI/UX and complex state management with Redux. This app fetches news data via APIs, offering previews and aplatform for user comments, all elegantly styled with CSS.</p>
+<p>1## News Reader Project: I&apos;ve brought the world of news to life through a React.js web app, masterfully handling UI/UX and complex state management with Redux. This app fetches news data via APIs, offering previews and a platform for user comments, all elegantly styled with CSS.</p>
 <br/>
-<p>2## Expenses Tracker: Simplifying financial management, my React.js web app empowers users to budget theirdaily expenditures with ease. Redux handles complex state management, while CSS ensures a sleek and intuitiveinterface.</p>
+<p>2## Expenses Tracker: Simplifying financial management, my React.js web app empowers users to budget their daily expenditures with ease. Redux handles complex state management, while CSS ensures a sleek and intuitive interface.</p>
 <br/>
-<p>3## Online Store: Creating an immersive e-commerce experience, I've built a web application where users canshop for products online. With the ability to toggle between multiple currencies and an efficient cart system, thisReact.js-based app showcases my prowess in UI/UX, state management, and CSS styling.</p>
+<p>3## Online Store: Creating an immersive e-commerce experience, I&apos;ve built a web application where users canshop for products online. With the ability to toggle between multiple currencies and an efficient cart system, this React.js-based app showcases my prowess in UI/UX, state management, and CSS styling.</p>
 <br/>
-<p>4## Jammming: Bringing music to life, I've crafted a web app that leverages Spotify's vast library. Users canseamlessly search for their favorite jams, create custom playlists, and save them to their Spotify accounts. It's allabout delivering a delightful UI/UX with React.js and CSS magic.</p>
+<p>4## Jammming: Bringing music to life, I&apos;ve crafted a web app that leverages Spotify&apos;s vast library. Users canseamlessly search for their favorite jams, create custom playlists, and save them to their Spotify accounts. It&apos;s allabout delivering a delightful UI/UX with React.js and CSS magic.</p>
 <br/>
 <p># My toolkit includes a robust set of skills:</p>
 <br/>
@@ -32,7 +32,7 @@ export default function About() {
 <li>Proficiency in JavaScript | Python3 | CSS | HTML</li>
 </ul>
 <br/>
-<p>As I continue to evolve and explore new horizons in the ever-evolving world of web development, I'm excitedto connect with fellow professionals, collaborate on exciting projects, and contribute to the digital landscape in</p>
+<p>As I continue to evolve and explore new horizons in the ever-evolving world of web development, I&apos;m excited to connect with fellow professionals, collaborate on exciting projects, and contribute to the digital landscape in</p>
         </div>
         </div>
         </>
